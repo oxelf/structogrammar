edd35dce0c982ed627aa4296309fefa5b9b8b9aa
oxelf <oxelf@users.noreply.github.com>
2024-09-04 19:43:17 +0000

    deploy: 423fee9fd791682e581071ff0378fa77ee87b52a

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a6bd3f1de158bb61090e0c8053df93a10cb548
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4280002695"
+    serviceWorkerVersion: "1950398383"
   }
 });
