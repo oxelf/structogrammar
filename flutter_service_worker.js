e7da177cd9d46cdc8cab9a616032510872f16bfa
oxelf <oxelf@users.noreply.github.com>
2024-09-12 13:32:45 +0000

    deploy: 2d1d0e3e9c213d8e59523ad9091ca4d7e068f0a9

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"main.dart.js": "ee119d65a358efa6535f2502bc67171a",
+const RESOURCES = {"main.dart.js": "3c8d1a9cacf7406a3a58100505cda6fd",
 "index.html": "080c37a9f613fe0c752e5583e28f02d5",
 "/": "080c37a9f613fe0c752e5583e28f02d5",
 "icons/Icon-512.png": "9d878943f868e35af7d413d85f9be13a",
@@ -21,7 +21,7 @@ const RESOURCES = {"main.dart.js": "ee119d65a358efa6535f2502bc67171a",
 "assets/packages/community_material_icon/fonts/materialdesignicons-webfont.ttf": "84c7bd136590da0a6ed2c21df180c354",
 "assets/AssetManifest.bin.json": "443a9c8fdd20d12ff8fdaf9105c5df23",
 "assets/AssetManifest.bin": "e50efe658e3fd7a32ab5f2829564d4e8",
-"flutter_bootstrap.js": "56246dc48ca29c392481f5a833617aeb",
+"flutter_bootstrap.js": "30cf6f3cbd36a2d7690d6a08ac3d2d54",
 "canvaskit/skwasm.js": "694fda5704053957c2594de355805228",
 "canvaskit/chromium/canvaskit.wasm": "b1ac05b29c127d86df4bcfbf50dd902a",
 "canvaskit/chromium/canvaskit.js": "671c6b4f8fcc199dcc551c7bb125f239",
